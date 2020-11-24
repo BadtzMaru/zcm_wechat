9b4dd42c629e0281677242d48fd302b2bc9902ff
BadtzMaru <707380414@qq.com>
2020-11-24 23:28:33 +0800

    引入自定义图标库

--- App.vue
+++ App.vue
@@ -1,17 +1,29 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
+export default {
+	onLaunch() {
+		// #ifdef APP-PLUS-NVUE
+		const domModule = weex.requireModule('dom');
+		domModule.addRule('fontFace', {
+			fontFamily: 'iconfont',
+			src: "url('https://at.alicdn.com/t/font_1365296_2ijcbdrmsg.ttf')"
+		});
+		// #endif
+		console.log('App Launch');
+	},
+	onShow() {
+		console.log('App Show');
+	},
+	onHide() {
+		console.log('App Hide');
 	}
+};
 </script>
 
 <style>
-	/*每个页面公共css */
+/*每个页面公共css */
+@import './common/free.css';
+@import './common/common.css';
+/* #ifndef APP-PLUS-NVUE */
+@import './common/free-icon.css';
+/* #endif */
 </style>
